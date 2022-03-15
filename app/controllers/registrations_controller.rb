4c8993e06b8b7fb3e863034c975d8a9acb25a895
Midelity <789xxbrennm@gmail.com>
2022-03-15 16:00:36 -0500

    fix fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,14 +6,14 @@ class RegistrationsController < ApplicationController
     def create
         #render plain: params
         #render plain: params[:user]
-        #render plain: "You data is MINE!!!"
-        @user = User.new(params[:user_params])
-        if @user.save
-            cookies[:user_id] = @user.id
-            redirect_to root_path, notice: "Sucessfully created an account"
-        else
-            render :new, status: :unprocessable_entity
-        end
+        render plain: "You data is MINE!!!"
+        # @user = User.new(params[:user_params])
+        # if @user.save
+        #     cookies[:user_id] = @user.id
+        #     redirect_to root_path, notice: "Sucessfully created an account"
+        # else
+        #     render :new, status: :unprocessable_entity
+        # end
     end
 
     private
