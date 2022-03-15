85b737263db781d0a6f3509f5f9ad368c5bf7b7e
Midelity <789xxbrennm@gmail.com>
2022-03-15 18:56:41 -0500

    login and sign up have been added to navbar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,11 @@ class RegistrationsController < ApplicationController
         #render plain: "You data is MINE!!!"
         @user = User.new(user_params)
         if @user.save
+            # this is bad as user_id could easily be changed
+            #cookies[:user_id]
+            # so we use session cookies to sogn in the user!
+            #server side it can be messed with but browser side it cannto be messed with
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Account created"
         else 
             render :new, status: :unprocessable_entity
